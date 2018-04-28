fa550985eb006040e3c92f53e481debab77d352f
Jeremy "EndenDragon" Zhang <jeremyzhang5000@gmail.com>
2018-04-28 00:09:42 -0700

    Update liveSurvey port

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(1949, function(){
+  console.log('listening on *:1949');
 });
